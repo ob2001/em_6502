e0cdeb18e369f8c935b54ccef683411a7618c979
ob2001 <butlero2001@outlook.com>
2025-10-27 22:31:24 -0400

    Handle error when building cpu memory.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use em_6502::cpu::{CPU6502, mem_with_code_at, mem_insert_code_at};
 
 fn main() {
     let mut cpu = CPU6502::new(true);
-    let mut mem = mem_with_code_at(&[0xA9, 0xFF, 0x00], 0).unwrap();
-    let _ = mem_insert_code_at(&mut mem, &[0x00, 0x00, 0x01, 0x00], 0xFFFC);
+    let mut mem = mem_with_code_at(&[0xA9, 0xF3, 0x00, 0xFF], 0).unwrap();
+    if let Err(e) = mem_insert_code_at(&mut mem, &[ 0x03, 0x00], 0xFFFE) {
+        panic!("{}", e);
+    }
 
     cpu.flash_mem(mem);
-    cpu.power_on_and_run();
 
+    cpu.power_on_and_run();
     // println!("{:?}", cpu);
 }

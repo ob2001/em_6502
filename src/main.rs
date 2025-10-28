5362c4e13c9d1b93bb2b5a2fe8a4dd3062507eb0
ob2001 <butlero2001@outlook.com>
2025-10-28 00:01:52 -0400

    Only need to unwrap

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use em_6502::cpu::{CPU6502, mem_with_code_at, mem_insert_code_at};
 fn main() {
     let mut cpu = CPU6502::new(true);
     let mut mem = mem_with_code_at(&[0xA9, 0xF3, 0x00, 0xFF], 0).unwrap();
-    if let Err(e) = mem_insert_code_at(&mut mem, &[ 0x03, 0x00], 0xFFFE) {
-        panic!("{}", e);
-    }
+    mem_insert_code_at(&mut mem, &[ 0x03, 0x00], 0xFFFE).unwrap();
 
     cpu.flash_mem(mem);
 

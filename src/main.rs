67b9ca802bca8468f199f93992784adc41a03331
ob2001 <butlero2001@outlook.com>
2025-10-27 20:35:06 -0400

    Create a small program and run it on cpu for test purposes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use em_6502::cpu::CPU6502;
+use em_6502::cpu::{CPU6502, mem_with_code_at, mem_insert_code_at};
 
 fn main() {
-    let _ = CPU6502::new();
+    let mut cpu = CPU6502::new(true);
+    let mut mem = mem_with_code_at(&[0xA9, 0xFF, 0x00], 0).unwrap();
+    let _ = mem_insert_code_at(&mut mem, &[0x00, 0x00, 0x01, 0x00], 0xFFFC);
 
-    println!("Hello, world!");
+    cpu.flash_mem(mem);
+    cpu.power_on_and_run();
+
+    // println!("{:?}", cpu);
 }

f6e9a3ccbc292930d7238f84577d1a62f322ea43
ob2001 <butlero2001@outlook.com>
2025-10-29 17:51:06 -0400

    Improve function naming; improve logic.

--- src/mem.rs
+++ src/mem.rs
@@ -1,29 +1,59 @@
 use crate::prelude::*;
 
-/// Return new zeroed cpu memory.
-pub fn mem_zeros() -> [CPUByte; CPU_MEMSIZE] {
-    [0; CPU_MEMSIZE]
+pub type MemResult = Result<[CPUByte; CPU_MEMSIZE], &'static str>;
+
+/// Create new cpu memory with all bytes set to the provided value
+pub fn new_all(b: CPUByte) -> [CPUByte; CPU_MEMSIZE] {
+    [b; CPU_MEMSIZE]
+}
+
+/// Create new cpu memory with all NOP instructions
+pub fn new_nops() -> [CPUByte; CPU_MEMSIZE] {
+    new_all(0x00)
+}
+
+/// Create new cpu memory with all (illegal) HLT instructions
+pub fn new_hlts() -> [CPUByte; CPU_MEMSIZE] {
+    new_all(0xFF)
 }
 
 /// Return new zeroed cpu memory with `code` inserted in range `mem[idx]` - `mem[idx + code.len()`.
-pub fn mem_with_code_at(code: &[CPUByte], idx: usize) -> Result<[CPUByte; CPU_MEMSIZE], &'static str> {
-    let mut ret = mem_zeros();
-    if idx <= CPU_MEMSIZE && code.len() + idx <= CPU_MEMSIZE {
+pub fn new_nops_with_code_at(code: &[CPUByte], idx: usize) -> MemResult {
+    if idx > CPU_MEMSIZE {
+        Err("Error: code insertion point out of memory bounds")
+    } else if code.len() + idx > CPU_MEMSIZE {
+        Err("Error: code overflows memory bounds. Consider inserting at an earlier index")
+    } else {
+        let mut ret = new_nops();
+
         for (offset, &byte) in code.iter().enumerate() {
             ret[idx + offset] = byte;
         }
+
         Ok(ret)
-    } else if idx > CPU_MEMSIZE {
+    }
+}
+
+pub fn new_hlts_with_code_at(code: &[CPUByte], idx: usize) -> MemResult {
+    if idx > CPU_MEMSIZE {
         Err("Error: code insertion point out of memory bounds")
-    } else {
+    } else if code.len() + idx > CPU_MEMSIZE {
         Err("Error: code overflows memory bounds. Consider inserting at an earlier index")
+    } else {
+        let mut ret = new_hlts();
+
+        for (offset, &byte) in code.iter().enumerate() {
+            ret[idx + offset] = byte;
+        }
+
+        Ok(ret)
     }
 }
 
 /// Modify existing cpu memory `mem` by inserting `code` in range `mem[idx]` - `mem[idx + code.len()]`.
 ///
 /// Will overwrite memory in modification range.
-pub fn mem_insert_code_at(mem: &mut [CPUByte; CPU_MEMSIZE], code: &[CPUByte], idx: usize) -> Result<(), &'static str> {
+pub fn insert_code_at(mem: &mut [CPUByte; CPU_MEMSIZE], code: &[CPUByte], idx: usize) -> Result<(), &'static str> {
     if idx <= CPU_MEMSIZE && code.len() + idx <= CPU_MEMSIZE {
         for (offset, &byte) in code.iter().enumerate() {
             mem[idx + offset] = byte;

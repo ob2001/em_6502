ee622237b9a2d4e46773d713cf75710cc4bfbeed
ob2001 <butlero2001@outlook.com>
2025-10-29 17:51:18 -0400

    Consequential/trivial changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use em_6502::{cpu::CPU6502, mem};
 
 fn main() {
-    let mut mem = mem::mem_with_code_at(&[0xA9, 0xF3, 0x00, 0xFF], 0).unwrap();
-    mem::mem_insert_code_at(&mut mem, &[0x03, 0x00], 0xFFFE).unwrap();
+    let mut mem = mem::new_nops_with_code_at(&[0xA9, 0xF3, 0x00, 0xFF], 0x0000).unwrap();
+    mem::insert_code_at(&mut mem, &[0xFF], 0x0010).unwrap();
+    mem::insert_code_at(&mut mem, &[0x00, 0x00, 0x10, 0x00], 0xFFFC).unwrap();
 
     let mut cpu = CPU6502::new_with_mem(mem);
     cpu.power_on_and_run(true);
 
     // Print cpu final state and dump memory
-    if false {
+    if true {
+        println!("*** Post execution cpu and mem dump ***");
         println!("{:?}", cpu);
     }
 }

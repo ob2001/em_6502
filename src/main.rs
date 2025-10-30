9e4db70b9ba750c0c189291f863bc74441f51021
ob2001 <butlero2001@outlook.com>
2025-10-30 18:05:24 -0400

    Allow generating CPU memory from a text file specifying code locations and values, and memory padding values.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use em_6502::{cpu::CPU6502, mem};
+use em_6502::cpu::CPU6502;
 
 fn main() {
-    let mut mem = mem::new_nops_with_code_at(&[0xA9, 0xF3, 0x00, 0xFF], 0x0000).unwrap();
-    mem::insert_code_at(&mut mem, &[0xFF], 0x0010).unwrap();
-    mem::insert_code_at(&mut mem, &[0x00, 0x00, 0x10, 0x00], 0xFFFC).unwrap();
+    let mut cpu = CPU6502::new_with_mem_from_file("in.txt".to_string()).unwrap();
+    cpu.set_allow_hlt(false);
+    cpu.set_illegal_opcode_mode(true);
+    cpu.set_cycle_limit(10);
 
-    let mut cpu = CPU6502::new_with_mem(mem);
     cpu.power_on_and_run(true);
 
     // Print CPU and memory postmortem
--- src/mem.rs
+++ src/mem.rs
@@ -51,6 +51,52 @@ pub fn new_hlts_with_code_at(code: &[CPUByte], idx: usize) -> MemResult {
     }
 }
 
+/// First iteration of a function to generate CPU memory from a file specification
+pub fn new_from_file(file: String) -> MemResult {
+    if let Ok(f) = std::fs::read_to_string(file) {
+        let mut ret = new_nops();
+        let mut radix_mode: u32 = 16;
+
+        for line in f.lines() {
+            let trim_line = line.trim();
+
+            if trim_line.len() == 0 {
+                continue;
+            }
+
+            match trim_line.as_bytes()[0] as char {
+                ';' => continue,
+                _ => {},
+            };
+
+            let (pre, post) = trim_line.split_once(':').unwrap();            
+            let mut post = post.split(',');
+
+            match pre {
+                "pad_all" => {
+                    ret = new_all(u8::from_str_radix(post.next().unwrap().trim(), 16).unwrap_or_else(|_| panic!("Invalid value for padding bytes")));
+                },
+                "radix_mode" => {
+                    radix_mode = post.next().unwrap().trim().parse::<u32>().unwrap_or_else(|_| panic!("Invalid radix mode"));
+                },
+                a => {
+                    let addr = u16::from_str_radix(a.trim(), radix_mode).unwrap_or_else(|_| panic!("Invalid address predicate: {}", pre));
+                    let mut mem_line: Vec<u8> = Vec::new();
+                    for b in post {
+                        mem_line.push(u8::from_str_radix(b.trim(), radix_mode).unwrap_or_else(|_| panic!("Invalid number representation: {} for radix mode {}", b, radix_mode)));
+                    }
+
+                    insert_code_at(&mut ret, &mem_line, addr as usize)?;
+                }
+            }
+        }
+
+        Ok(ret)
+    } else {
+        MemResult::Err("")
+    }
+}
+
 /// Modify existing cpu memory `mem` by inserting `code` in range `mem[idx]` - `mem[idx + code.len()]`.
 ///
 /// Will overwrite memory in modification range.

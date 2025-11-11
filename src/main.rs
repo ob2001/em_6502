41d4c476ded1f2918561712d761889da91b2df71
ob2001 <butlero2001@outlook.com>
2025-11-10 19:37:17 -0500

    Better initial control flow. Implement crossterm commands in a better way.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-#![allow(unused_variables)]
-
-use std::io::{stdin, stdout};
+use std::io;
 use crossterm::{
-    execute,
+    ExecutableCommand,
     cursor::MoveTo,
     style::Print,
     terminal::{
@@ -16,37 +14,45 @@ use crossterm::{
 use em_6502::{cpu::CPU6502, mem::Mem};
 
 fn main() {
-    let mut interactive = false;
-    let mut args = std::env::args();
-    if let Some(v) = args.nth(1) {
-        match v.as_str() {
-            "i" | "-i" | "--int" | "--interactive" => interactive = true,
-            _ => {},
+    if let Some(mode) = std::env::args().nth(1) {
+        match mode.as_str() {
+            "d" | "-d" | "--dbg" | "--debug" => {
+                let mut cpu = CPU6502::new_with_mem_from_file("in_sample_2.txt".to_string()).unwrap();
+                cpu.set_allow_hlt(true);
+                cpu.set_illegal_opcode_mode(true);
+                cpu.set_cycle_limit(0);
+
+                cpu.power_on_and_run(true);
+
+                if false {
+                    println!("vvv CPU and Memory Postmortem vvv");
+                    println!("{:?}", cpu);
+                }
+            },
+            "i" | "-i" | "--int" | "--interactive" => {
+                _ = interactive();
+            },
+            "h" | "-h" | "--help" => {
+                print_help();
+                return
+            }
+            _ => panic!("Incorrect usage - invalid program mode \"{}\"\nUse [--help] flag for usage instructions", mode),
         }
-    }
-
-    if interactive {
-        interactive_ui();
     } else {
-        let mut cpu = CPU6502::new_with_mem_from_file("in_sample_2.txt".to_string()).unwrap();
-        cpu.set_allow_hlt(true);
-        cpu.set_illegal_opcode_mode(true);
-        cpu.set_cycle_limit(0);
-
-        cpu.power_on_and_run(true);
-
-        println!("vvv CPU and Memory Postmortem vvv");
-        println!("{:?}", cpu);
+        panic!("Incorrect usage - please specify a program mode\nUse [--help] flag for usage instructions");
     }
 }
 
-fn interactive_ui() {
-    _ = execute!(
-        stdout(),
-        EnterAlternateScreen,
-        Clear(ClearType::All),
-        MoveTo(0, 0)
-    );
+fn print_help() {
+    todo!("program usage information to be implemented")
+}
+
+fn interactive() -> std::io::Result<()> {
+    let mut stdout = io::stdout();
+    
+    stdout.execute(EnterAlternateScreen)?
+    .execute(Clear(ClearType::All))?
+    .execute(MoveTo(0, 0))?;
 
     let mut exit = false;
     let options = vec![
@@ -60,35 +66,36 @@ fn interactive_ui() {
     let mut cpu = CPU6502::new();
     
     while !exit {
-        _ = execute!(stdout(), Clear(ClearType::All), MoveTo(0, 0));
+        stdout.execute(Clear(ClearType::All))?
+        .execute(MoveTo(0, 0))?;
+
         let mut buf_in = String::new();
 
-        _ = execute!(
-            stdout(),
-            Print(format!("Current CPU state:\n\n{}\n\n", cpu)),
-            Print("Please select an option:\n")
-        );
+        stdout.execute(Print(format!("Current CPU state:\n\n{}\n\n", cpu)))?
+        .execute(Print("Please select an option:\n"))?;
 
         for (i, &opt) in options.iter().enumerate() {
-            _ = execute!(stdout(), Print(format!("{}: {}\n", i, opt)));
+            stdout.execute(Print(format!("{}: {}\n", i, opt)))?;
         }
         
-        _ = stdin().read_line(&mut buf_in);
+        _ = io::stdin().read_line(&mut buf_in);
         
         match buf_in.trim().parse::<usize>() {
             Ok(0) => exit = true,
-            Ok(1) => cpu = new_cpu(),
+            Ok(1) => cpu = new_cpu().unwrap(),
             Ok(2) => edit_cpu(&mut cpu),
             Ok(3) => run_cpu_steps(),
             Ok(4) => run_cpu_continuous(),
-            Ok(5) => print_to_file(&cpu),
-            _ => _ = execute!(stdout(), Print(format!("{} is not a valid option\n", buf_in.trim()))),
+            Ok(5) => print_to_file(&cpu).unwrap(),
+            _ => { stdout.execute(Print(format!("{} is not a valid option\n", buf_in.trim())))?; },
         }
     }
-    _ = execute!(std::io::stdout(), LeaveAlternateScreen);
+    stdout.execute(LeaveAlternateScreen)?;
+    Ok(())
 }
 
-fn new_cpu() -> CPU6502 {
+fn new_cpu() -> io::Result<CPU6502> {
+    let mut stdout = io::stdout();
     let mut valid = false;
     let mut buf_in = String::new();
     let cpu = CPU6502::new();
@@ -102,76 +109,77 @@ fn new_cpu() -> CPU6502 {
     ];
     
     while !valid {
-        _ = execute!(stdout(),
-            Clear(ClearType::All),
-            MoveTo(0, 0),
-            Print(format!("Current CPU state:\n\n{}\n\n", cpu))
-        );
+        stdout.execute(Clear(ClearType::All))?
+        .execute(MoveTo(0, 0))?
+        .execute(Print(format!("Current CPU state:\n\n{}\n\n", cpu)))?;
 
         valid = true;
         buf_in.clear();
 
-        _ = execute!(stdout(), Print("Select an option:\n"));
+        stdout.execute(Print("Select an option:\n"))?;
         for (i, &opt) in options.iter().enumerate() {
-            _ = execute!(stdout(), Print(format!("{}: {}\n", i, opt)));
+            stdout.execute(Print(format!("{}: {}\n", i, opt)))?;
         }
-        _ = stdin().read_line(&mut buf_in);
+        _ = io::stdin().read_line(&mut buf_in);
 
         match buf_in.trim().parse::<usize>() {
             Ok(0) => {},
-            Ok(1) => return CPU6502::new(),
-            Ok(2) => return CPU6502::new_with_mem(Mem::new_nops()),
-            Ok(3) => return CPU6502::new_with_mem(Mem::new_hlts()),
+            Ok(1) => return Ok(CPU6502::new()),
+            Ok(2) => return Ok(CPU6502::new_with_mem(Mem::new_nops())),
+            Ok(3) => return Ok(CPU6502::new_with_mem(Mem::new_hlts())),
             Ok(4) => {
                 buf_in.clear();
 
-                _ = execute!(stdout(), Print("Please input the file name: "));
-                _ = stdin().read_line(&mut buf_in);
+                stdout.execute(Print("Please input the file name: "))?;
+                _ = io::stdin().read_line(&mut buf_in);
 
                 if let Ok(mem) = Mem::new_from_file(buf_in.trim().to_string()) {
-                    return CPU6502::new_with_mem(mem);
+                    return Ok(CPU6502::new_with_mem(mem));
                 } else {
                     valid = false;
-                    _ = execute!(stdout(), Print(format!("Error loading memory from spec file \"{}\"", buf_in)));
+                    stdout.execute(Print(format!("Error loading memory from spec file \"{}\"", buf_in)))?;
                 }
             },
             _ => {
                 valid = false;
-                _ = execute!(stdout(), Print(format!("{} is not a valid option\n", buf_in.trim())));
+                stdout.execute(Print(format!("{} is not a valid option\n", buf_in.trim())))?;
             }
         }
-
     }
 
-    cpu
+    Ok(cpu)
 }
 
 fn edit_cpu(cpu: &mut CPU6502) {
     // todo
+    todo!("interactive cpu editing to be implemented");
 }
 
 fn run_cpu_steps() {
     // todo
+    todo!("running CPU in step mode to be implemented");
 }
 
 fn run_cpu_continuous() {
     // todo
+    todo!("running CPU in continuous mode to be implemented")
 }
 
-fn print_to_file(cpu: &CPU6502) {
+fn print_to_file(cpu: &CPU6502) -> io::Result<()> {
+    let mut stdout = io::stdout();
     let mut buf_in = String::new();
 
     loop { 
         buf_in.clear();
-        _ = execute!(stdout(), Print("Please specify file to output to, or type \"cancel\" to cancel: "));
-        _ = stdin().read_line(&mut buf_in);
+        stdout.execute(Print("Please specify file to output to, or type \"cancel\" to cancel: "))?;
+        _ = io::stdin().read_line(&mut buf_in);
 
-        if &buf_in.trim() == &"cancel" {
-            break;
+        if buf_in.trim() == "cancel" {
+            break Ok(());
         } else if let Ok(_) = std::fs::write(&buf_in.trim(), format!("{:?}", cpu)) {
-            break;
+            break Ok(());
         } else {
-            _ = execute!(stdout(), Print(format!("Error writing to file \"{}\"\n", &buf_in)));
+            stdout.execute(Print(format!("Error writing to file \"{}\"\n", &buf_in)))?;
         }
     }
 }
